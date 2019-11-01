97c161a6a355d620dc9ff0786486927d2b9e357d
MathiasSLarsen <mathias.s.larsen99@gmail.com>
2019-11-01 10:48:20 +0100

    Add files via upload

---
+++ CanteenTest1/CanteenTest1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CanteenTest1
+//
+//  Created by Mathias Larsen on 01/11/2019.
+//  Copyright © 2019 MadneZ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
